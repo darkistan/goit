70d4290ec2fed75659a7d1a06aaff9e2043b8add
Darkistan <91938225+darkistan@users.noreply.github.com>
2025-04-07 17:42:49 +0300

    Перевірка: допустимі значення — тільки в діапазоні [1, 1000] + []

--- goit-algo-hw-03/Task2.py
+++ goit-algo-hw-03/Task2.py
@@ -4,23 +4,26 @@ import random
 def get_numbers_ticket(min, max, quantity):
     # Перевірка типів
     if not all(isinstance(x, int) for x in [min, max, quantity]):
-        return "Помилка: всі вхідні значення мають бути цілими числами."
+        return []
 
     # Перевірка, що min > max
     if min > max:
-        return "Помилка: min не може бути більшим за max."
+        return []
 
     # Перевірка, що кількість чисел не перевищує кількість доступних значень
     if quantity > (max - min + 1):
-        return "Помилка: неможливо вибрати стільки унікальних чисел у заданому діапазоні."
+        return []
+
+    # Перевірка: допустимі значення — тільки в діапазоні [1, 1000]
+    if min < 1 or max > 1000:
+        return []
 
     # Перевірка на додатні значення
-    if quantity <= 0 or min < 0 or max < 0:
-        return "Помилка: значення мають бути додатними цілими числами."
+    if quantity <= 0:
+        return []
 
     # Генерація та сортування чисел
     numbers = sorted(random.sample(range(min, max + 1), quantity))
     return numbers
 
-print(get_numbers_ticket(1, 49, 6))
-
+print(get_numbers_ticket(1000, 1200, 10)) # Перевірка на допустимі значення
